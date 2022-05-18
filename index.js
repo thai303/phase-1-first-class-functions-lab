71aaa81a08c915f53db5607948f0981d88f02fdc
Thai Xiong <thai303@hotmail.com>
2022-05-17 19:53:41 -0600

    commit tree 26cfee782204685fded0dd714b0a1b09790e0459

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = (multiplyValue) => {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = (drivers, driversToReturn) => {
+    return driversToReturn(drivers);
+  };
